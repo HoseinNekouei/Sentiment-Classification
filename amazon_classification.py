ac8ae24a284456cf33b79f8ca3c53972a0cd7f0f
Hosein Nekouei <nekouei.hosein@gmail.com>
2024-03-07 14:09:26 +0330

    Added a docstring to the code and refactored it into an object-oriented approach

--- amazon_classification.py
+++ amazon_classification.py
@@ -11,109 +11,242 @@ import matplotlib.pyplot as plt
 from sklearn.utils.class_weight import compute_class_weight
 
 
-MAX_TOKEN =  7500
-MAX_LEN = 256
+MAX_TOKEN =  10000
+MAX_LEN = 512
 EPOCHS = 10
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 
-# Read cvs file in memory
-data = pd.read_csv('amazon_baby.csv', usecols=[1,2], nrows=90000)
+def load_text_dataset(path_info: str) -> tuple[np.array, np.array]:
+    '''
+        Load a CSV file from the specified directory.
 
-# drop null reviews
-empty_idxs = data[data['review'].isnull()].index
-data.drop(empty_idxs, inplace=True)
+        Args:
+            directory_path (str):
+                Path to the directory containing the text data.
 
-data = data.sample(frac=1)
-features = data['review'].astype(str)
-labels = data['rating'].apply(lambda x: 0 if x > 0 and x <=2 else 1)
+            batch_size(int): 
+                Number of samples per batch.
 
-print(labels[:100].tolist())
+        Return:
+            features (array): The features data.
+            labels (array): The labels data.
+    '''
 
-#compute class weights
-# The weight of the classes when taking into account the distribution of labels
-class_weights = compute_class_weight('balanced', classes= np.unique(labels), y= labels )
-class_weights= {i: class_weights[i] for i in range(2)}
-print(f'Labels weight: {class_weights}')
+    # Read cvs file in memory
+    data = pd.read_csv(path_info, usecols=[1,2])
 
-# Split dataset into training and testing dataset
-x_train, x_test, y_train, y_test = train_test_split(features,
-                                                    labels, 
-                                                    test_size=0.1, 
-                                                    random_state= 42)
-print(x_train.shape)
+    # drop null reviews
+    empty_idxs = data[data['review'].isnull()].index
+    data.drop(empty_idxs, inplace=True)
 
-# Create a TextVectorization layer
-vectorizer = layers.TextVectorization(max_tokens= MAX_TOKEN,
-                                                output_mode= 'int',
-                                                output_sequence_length= MAX_LEN)
+    # Shuffle the dataframe rows
+    data = data.sample(frac=1)
 
-# Adapt the TextVectorization layer to text data
-vectorizer.adapt(x_train)
+    # Split labels and features. Change labels > 2 to 0, others to 1
+    features = data['review'].astype(str)
+    labels = data['rating'].apply(lambda x: 0 if x > 0 and x <=2 else 1)
 
-print(vectorizer.get_vocabulary()[:10])
+    # Print the shuffled dataframe for the top 100 records 
+    print(labels[:100].tolist())# Read cvs file in memory
+    data = pd.read_csv('amazon_baby.csv', usecols=[1,2])
 
-# Define input layer
-inputs = keras.Input(shape = (1,), dtype = tf.string)
+    # drop null reviews
+    empty_idxs = data[data['review'].isnull()].index
+    data.drop(empty_idxs, inplace=True)
 
-# vectorize input
-vectorize = vectorizer(inputs)
+    # Shuffle the dataframe rows
+    data = data.sample(frac=1)
 
-# Embedding layer
-embeded= layers.Embedding(input_dim= MAX_TOKEN, output_dim= 256)(vectorize)
+    # Split labels and features. Change labels > 2 to 0, others to 1
+    features = data['review'].astype(str)
+    labels = data['rating'].apply(lambda x: 0 if x > 0 and x <=2 else 1)
 
-# create LSTM model
-x = layers.LSTM(units= 64, recurrent_dropout=0.5, return_sequences=True)(embeded)
-x = layers.LSTM(units= 32, recurrent_dropout=0.5, return_sequences=True)(x)
-x = layers.LSTM(units= 32, recurrent_dropout=0.5)(x)
-x = layers.Dropout(0.5)(x)
+    # Print the shuffled dataframe for the top 100 records 
+    print(labels[:100].tolist())
 
-# Output layer
-outputs = layers.Dense(1, activation='sigmoid')(x)
+    return (features, labels)
 
-# Create Model
-model = Model(inputs, outputs)
 
-# Define optimizer with custom learning rate
-opt =  keras.optimizers.Adam(learning_rate= 0.1)
+def preprocess_text_data(features, labels):
 
-# Compile the model
-model.compile(optimizer= opt,
-              loss = 'binary_crossentropy',
-              metrics= 'accuracy',
-              )
+    '''
+        get features and labels and preprocesses text data from the specified datasets.
 
-model.summary()
+        Args:
+            Features:
+                Features dataset containing text samples.
 
-history = model.fit(x_train, 
-                    y_train, 
-                    validation_data= (x_test, y_test), 
-                    epochs = EPOCHS, 
-                    batch_size= BATCH_SIZE,
-                    class_weight= class_weights)
+            Labels:
+                Labels that containing true integer samples.
 
-loss, accuracy = model.evaluate(x_test, y_test) 
-print(f'Test loss: {loss :.2f}, Test accuracy: {accuracy :.2f}')
+            Max_token (int): 
+                Maximum number of tokens in the vocabulary.
 
-# save model
-model.save("amazon_model", save_format='tf')
+            Output_mode (str):
+                Output mode ('int' for integer-encoded tokens).
 
-plt.style.use('ggplot')
+            Output_sequence_length (int):
+                Maximum sequence length for the output (length of the dictionary).
 
-# Get the loss and accuracy values from the history object
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-accuracy = history.history['accuracy']
-val_accuracy = history.history['val_accuracy']
+        Returns:
+            x_train (array): 
+                The training data.
 
-plt.plot(np.arange(EPOCHS), loss, label='Loss')
-plt.plot(np.arange(EPOCHS), val_loss, label= 'Val_loss')
+            x_test (array): 
+                The testing data.
 
-plt.plot(np.arange(EPOCHS), accuracy, label='accuracy')
-plt.plot(np.arange(EPOCHS), val_accuracy, label= 'Val_accuracy')
+            y_train (array): 
+                The training target.
 
-plt.legend()
-plt.xlabel('Epotch')
-plt.ylabel('Loss/Accuracy')
-plt.title('Amazon sentiment analysis')
+            y_test (array): 
+                The testing target.
 
-plt.show()
+            class_weights (dict): 
+                A dictionary mapping class indices to their respective weights.
+    '''
+
+    #compute class weights when taking into account the distribution of labels
+    class_weights = compute_class_weight('balanced', classes= np.unique(labels), y= labels )
+    class_weights= {i: class_weights[i] for i in range(2)}
+    print(f'Labels weight: {class_weights}')
+
+    # Split dataset into training and testing dataset
+    x_train, x_test, y_train, y_test = train_test_split(features,
+                                                        labels, 
+                                                        test_size=0.1, 
+                                                        random_state= 42)
+    print(f'x_train.shape: {x_train.shape}')
+    print(f'x_test.shape: {x_test.shape}')
+    print(f'y_tain.shape: {y_train.shape}')
+    print(f'y_test.shape: {y_test.shape}')
+    print(x_train.iloc[0])
+
+    # Create a TextVectorization layer
+    vectorizer = layers.TextVectorization(max_tokens= MAX_TOKEN,
+                                                    output_mode= 'int',
+                                                    output_sequence_length= MAX_LEN)
+
+    # Adapt the TextVectorization layer to text data
+    vectorizer.adapt(x_train)
+
+    x_train = x_train.map(lambda x: vectorizer(x))
+    x_test = x_train.map(lambda x: vectorizer(x))
+
+    print(vectorizer.get_vocabulary()[:10])    
+
+    return (vectorizer, class_weights, x_train, x_test, y_train, y_test)
+
+
+def algorithm(vectorizer, class_weights, x_train, x_test, y_train, y_test):
+    '''
+        Create an LSTM_based binary classification model.
+
+        Args:
+            max_token (int):
+                Maximun number of tokens in the vocabulary.
+            
+        returns:
+            history (keras.callbacks.History): 
+                A History object containing training/validation loss and metrics.
+    '''
+
+    # Define input layer
+    inputs = keras.Input(shape = (1,), dtype = tf.string)
+
+    # vectorize input
+    vectorize = vectorizer(inputs)
+
+    # Embedding layer
+    embeded= layers.Embedding(input_dim= MAX_TOKEN, output_dim= 256)(vectorize)
+
+    # create LSTM model
+    x = layers.LSTM(units= 64, recurrent_dropout=0.5, return_sequences=True)(embeded)
+    x = layers.LSTM(units= 32, recurrent_dropout=0.5, return_sequences=True)(x)
+    x = layers.LSTM(units= 32, recurrent_dropout=0.5)(x)
+    x = layers.Dropout(0.5)(x)
+
+    # Output layer
+    outputs = layers.Dense(1, activation='sigmoid')(x)
+
+    # Create Model
+    model = Model(inputs, outputs)
+
+    # Define optimizer with custom learning rate
+    opt =  keras.optimizers.Adam(learning_rate= 0.01)
+
+    # Compile the model
+    model.compile(optimizer= opt,
+                loss = 'binary_crossentropy',
+                metrics= 'accuracy')
+
+    model.summary()
+
+    history = model.fit(x_train, 
+                        y_train, 
+                        validation_data= (x_test, y_test), 
+                        epochs = EPOCHS, 
+                        batch_size= BATCH_SIZE,
+                        class_weight= class_weights)
+
+    loss, accuracy = model.evaluate(x_test, y_test) 
+    print(f'Test loss: {loss :.2f}, Test accuracy: {accuracy :.2f}')
+
+    # save model
+    model.save("amazon_model", save_format='tf')
+
+    return history
+
+
+def show_results(history):
+    
+    """
+    Plot the training and validation loss/accuracy from the history object.
+
+    Args:
+        history (keras.callbacks.History): 
+            The history object returned from model training.
+        
+        EPOCHS (int): The number of epochs for which the model was trained.
+
+    Returns:
+        None
+    """
+    
+    # Set the plot style to 'ggplot'
+    plt.style.use('ggplot')
+
+    # Extract loss and accuracy values from the history object
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    accuracy = history.history['accuracy']
+    val_accuracy = history.history['val_accuracy']
+
+    # Plot the training and validation loss
+    plt.plot(np.arange(EPOCHS), loss, label='Loss')
+    plt.plot(np.arange(EPOCHS), val_loss, label= 'Val_loss')
+
+    # Plot the training and validation accuracy
+    plt.plot(np.arange(EPOCHS), accuracy, label='accuracy')
+    plt.plot(np.arange(EPOCHS), val_accuracy, label= 'Val_accuracy')
+
+    # Add legend, labels, and title to the plot
+    plt.legend()
+    plt.xlabel('Epotch')
+    plt.ylabel('Loss/Accuracy')
+    plt.title('Amazon sentiment analysis')
+
+    # Display the plot
+    plt.show()
+
+
+def main():
+    print('The main function is runnig')
+
+    feature, labels = load_text_dataset(r'~/amazon_baby.csv')
+    vectorizer, class_weights, x_train, x_test, y_train, y_test  = preprocess_text_data(feature, labels)
+    history = algorithm(vectorizer, class_weights, x_train, x_test, y_train, y_test)
+    show_results(history)
+
+
+if __name__== '__main__': 
+    main()

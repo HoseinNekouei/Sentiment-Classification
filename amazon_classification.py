2c1ba23c6168a7456a90f3985f2c9a17c6d472a6
Hosein Nekouei <nekouei.hosein@gmail.com>
2024-03-07 12:03:47 +0330

    shuffled data by adding df.sample()

--- amazon_classification.py
+++ amazon_classification.py
@@ -17,27 +17,29 @@ EPOCHS = 10
 BATCH_SIZE = 32
 
 # Read cvs file in memory
-data = pd.read_csv('amazon_baby.csv', usecols=[1,2], nrows=20000)
+data = pd.read_csv('amazon_baby.csv', usecols=[1,2], nrows=90000)
 
 # drop null reviews
 empty_idxs = data[data['review'].isnull()].index
 data.drop(empty_idxs, inplace=True)
 
+data = data.sample(frac=1)
 features = data['review'].astype(str)
-lables = data['rating'].apply(lambda x: 0 if x > 0 and x <=2 else 1)
+labels = data['rating'].apply(lambda x: 0 if x > 0 and x <=2 else 1)
+
+print(labels[:100].tolist())
 
 #compute class weights
 # The weight of the classes when taking into account the distribution of labels
-class_weights = compute_class_weight('balanced', classes= np.unique(lables), y= lables )
+class_weights = compute_class_weight('balanced', classes= np.unique(labels), y= labels )
 class_weights= {i: class_weights[i] for i in range(2)}
 print(f'Labels weight: {class_weights}')
 
 # Split dataset into training and testing dataset
 x_train, x_test, y_train, y_test = train_test_split(features,
-                                                    lables, 
-                                                    test_size=0.2, 
-                                                    random_state= 42, 
-                                                    shuffle=True)
+                                                    labels, 
+                                                    test_size=0.1, 
+                                                    random_state= 42)
 print(x_train.shape)
 
 # Create a TextVectorization layer
@@ -60,9 +62,9 @@ vectorize = vectorizer(inputs)
 embeded= layers.Embedding(input_dim= MAX_TOKEN, output_dim= 256)(vectorize)
 
 # create LSTM model
-x = layers.LSTM(units= 64, recurrent_dropout=0.5, return_sequences=True, activation='relu')(embeded)
-x = layers.LSTM(units= 32, recurrent_dropout=0.5, return_sequences=True, activation='relu')(x)
-x = layers.LSTM(units= 32, recurrent_dropout=0.5, activation='relu')(x)
+x = layers.LSTM(units= 64, recurrent_dropout=0.5, return_sequences=True)(embeded)
+x = layers.LSTM(units= 32, recurrent_dropout=0.5, return_sequences=True)(x)
+x = layers.LSTM(units= 32, recurrent_dropout=0.5)(x)
 x = layers.Dropout(0.5)(x)
 
 # Output layer
